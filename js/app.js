da0ae8286b9574cb373a2ef87bfe418f639a40fc
Cenora6 <aleksandra.gasidlo@gmail.com>
2020-02-13 19:55:06 +0100

    Added radio styling and animation

---
+++ js/app.js
@@ -0,0 +1,68 @@
+$(function () {
+
+    const sizeForm = $('.size');
+    const sizeChoose = sizeForm.find('.choose');
+    const small = sizeForm.find('input#small');
+    const medium = sizeForm.find('input#medium');
+    const large = sizeForm.find('input#large');
+    const extraLarge = sizeForm.find('input#extra');
+    const image = sizeForm.find('img');
+
+    sizeChoose.css({
+        left: "0.2rem",
+    });
+
+    image.css({
+        width: "5rem",
+    });
+
+    small.on('click', function () {
+        sizeChoose.css({
+            top: "0",
+            left: "0.2rem",
+            width: "1.5rem",
+            height: "1.5rem",
+        });
+        image.css({
+            width: "5rem",
+        });
+    });
+
+    medium.on('click', function () {
+        sizeChoose.css({
+            top: "-0.6rem",
+            left: "6rem",
+            width: "2rem",
+            height: "2.5rem",
+        });
+        image.css({
+            width: "6rem",
+        });
+    });
+
+    large.on('click', function () {
+        sizeChoose.css({
+            top: "-1rem",
+            left: "13rem",
+            width: "2.5rem",
+            height: "3.8rem",
+        });
+        image.css({
+            width: "7rem",
+        });
+    });
+
+    extraLarge.on('click', function () {
+        sizeChoose.css({
+            top: "-1.5rem",
+            left: "20rem",
+            width: "3rem",
+            height: "5rem",
+        });
+        image.css({
+            width: "8rem",
+        });
+    })
+
+
+});
